08e60ab5fe65f5efb4e1491e2df4f7f21b16061a
BrickCat <549595297@qq.com>
2018-03-08 13:03:13 +0800

    init

---
+++ App/src/main/java/club/mancool/mall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package club.mancool.mall.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import club.mancool.mall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
